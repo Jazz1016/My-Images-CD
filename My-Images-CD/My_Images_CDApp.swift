e9b741cf0ef299e0847471111e1aff4f6fc5ae49
James Lea <james.q.lea@gmail.com>
2023-06-02 09:20:58 -0600

    first commit

---
+++ My-Images-CD/My_Images_CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Images_CDApp.swift
+//  My-Images-CD
+//
+//  Created by James Lea on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct My_Images_CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
